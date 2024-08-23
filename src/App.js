fe7c401c50a273c2673916928a3828fd0437c023
azsheyeet <azrealyeno@gmail.com>
2024-08-23 13:53:48 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DCU <code>HUB</code> TESTING RAAAWR.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TEST LINK MUGANA SYA RAWR
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,4 @@
+
+<style type="text/css">
+  #div-with-bg {background - image}: url('buwan_ng_wika.png');
+</style>;
