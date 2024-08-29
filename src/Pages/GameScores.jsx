cc9910577e761ed85ff281ff0af0474c550fce99
JsonJin <skyogenhotau@gmail.com>
2024-08-29 18:16:33 +0800

    Scoreboardd

--- src/Pages/GameScores.jsx
+++ src/Pages/GameScores.jsx
@@ -1,27 +1,32 @@
 
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-import { streamScores, sortRanks , getGame} from '../dataService';
-import {medal_img} from '../constants';
+import { streamScores, sortRanks, getGame } from '../dataService';
+import { medal_img } from '../constants';
+import { Ename_img } from '../constants';
 import bgNeutral from '../assets/background.png';
 import './GameScores.css';
 
 const GameScores = () => {
     const [game, setGame] = useState([]);
     const [ranks, setRanks] = useState([]);
-    const [winner, setWinner] = useState({id: "",
+    const [winner, setWinner] = useState({
+        id: "",
         name: "",
-        backgroundImage: bgNeutral,});
+        backgroundImage: bgNeutral,
+    });
     const { gameid } = useParams();
 
-    const getWinner = (rankedScores) =>  {
-        var winner = {id: "",
+    const getWinner = (rankedScores) => {
+        var winner = {
+            id: "",
             name: "",
-            backgroundImage: bgNeutral,};
+            backgroundImage: bgNeutral,
+        };
         var winnerScore = 0;
         for (let index = 0; index < rankedScores.length; index++) {
             const element = rankedScores[index];
-            if(element.score > 0 && element.score > winnerScore){
+            if (element.score > 0 && element.score > winnerScore) {
                 winner = element.empire;
                 winnerScore = element.score;
             }
@@ -45,26 +50,26 @@ const GameScores = () => {
             const unsubscribe = streamScores(gameid,
                 (querySnapshot) => {
                     const scores = querySnapshot.docs
-                        .map((docSnapshot) => 
-                            ({...docSnapshot.data(), id:docSnapshot.id })
-                        ) ;
-                        //console.log('scores',scores);
-                        
-                        //moagi lang diri.. kay ambot nganong dili pwede i-deretso array
-                        const newScores = [];
-                        for (let index = 0; index < scores.length; index++) {
-                            const score = scores[index];
-                            newScores[score.id] = score.score;
-                        }
-                        //console.log('newScores',newScores);
-                        
-                        const rankedScores = sortRanks(newScores);
-                        //console.log('rankedScores',rankedScores);
-                        setRanks(rankedScores);
-                        const champion = getWinner(rankedScores);
-                        setWinner(champion);
+                        .map((docSnapshot) =>
+                            ({ ...docSnapshot.data(), id: docSnapshot.id })
+                        );
+                    //console.log('scores',scores);
 
+                    //moagi lang diri.. kay ambot nganong dili pwede i-deretso array
+                    const newScores = [];
+                    for (let index = 0; index < scores.length; index++) {
+                        const score = scores[index];
+                        newScores[score.id] = score.score;
                     }
+                    //console.log('newScores',newScores);
+
+                    const rankedScores = sortRanks(newScores);
+                    //console.log('rankedScores',rankedScores);
+                    setRanks(rankedScores);
+                    const champion = getWinner(rankedScores);
+                    setWinner(champion);
+
+                }
             );
             return unsubscribe;
         }
@@ -73,21 +78,21 @@ const GameScores = () => {
     }, [gameid, setRanks, setWinner])
 
     var divStyle = {
-        backgroundImage: 'url('+ winner.backgroundImage +')',
+        backgroundImage: 'url(' + winner.backgroundImage + ')',
         backgroundSize: "cover"
     };
 
     return (
-        
+
         <div className="App">
-            <div className="gameScores"  style={divStyle}>
+            <div className="gameScores" style={divStyle}>
                 <div className="game-content">
 
-                    <div>{game.name}</div>
-                    <div>{game.info}</div>
-                    
+                    <div className="game_Name">{game.name}</div>
+                    <div className="game_Info">{game.info}</div>
+
                     {
-                        ranks.map((rank) => <div key={rank.empireId} className="rank_container"> <div className="medal"><img src={medal_img[rank.medal]} className="medal_img" alt="medal_img"></img></div> {rank.empire.name} {rank.score} </div>)
+                        ranks.map((rank) => <div key={rank.empireId} className="rank_container"> <div className="medal"><img src={medal_img[rank.medal]} className="medal_img" alt="medal_img"></img></div><div className="Ename"><img src={Ename_img[rank.empire.name]} className="Ename_img" alt="Ename_img"></img></div> {rank.score} </div>)
                     }
 
                 </div>
--- src/constants.js
+++ src/constants.js
@@ -9,6 +9,11 @@ import img_silver from './assets/silver.png';
 import img_bronze from './assets/bronze.png';
 import img_consolation from './assets/consolation.png';
 
+import img_Pheonix from './assets/Empirename_Resilient_Phoenix.png';
+import img_Pegasus from './assets/Empirename_Prudent_Pegasus.png';
+import img_Chimera from './assets/Empirename_Revenant_Chimera.png';
+import img_Shinobi from './assets/Empirename_Shinobi_Beasts.png';
+
 
 export const empireIds = ["chimera","pegasus","phoenix","shinobi"];
 
@@ -41,5 +46,14 @@ export const medal_img = {
     "gold": img_gold,
     "silver": img_silver,
     "bronze": img_bronze,
-    "consolation": img_consolation
+    "consolation": img_consolation,
+}
+
+export const Ename = ["Phoenix", "Pegasus", "Chimera", "Shinobi"];
+
+export const Ename_img = {
+    "Phoenix": img_Pheonix,
+    "Pegasus": img_Pegasus,
+    "Chimera": img_Chimera,
+    "Shinobi": img_Shinobi,
 }
