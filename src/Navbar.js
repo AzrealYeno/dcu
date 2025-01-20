814d111b08b0a85d5b441b1f933ceaa6b8be9e90
JsonJin <skyogenhotau@gmail.com>
2025-01-20 21:20:50 +0800

    Nav Bar Burger Bar Addition

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-  
-  <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/livegames">LiveGames</NavLink>
-    <NavLink to="/admin">Admin</NavLink>
-  </nav>
-
-
-);
+    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+      <div className="burger-icon" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      
+      <div className={`nav-links ${isMenuOpen ? 'show' : ''}`}>
+        <NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+        <NavLink to="/livegames" onClick={() => setIsMenuOpen(false)}>LiveGames</NavLink>
+        <NavLink to="/admin" onClick={() => setIsMenuOpen(false)}>Admin</NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
